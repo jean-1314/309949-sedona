571c25c6951701efdc92abb00b37a6dd0c38ce5a
jean-1314 <isfiremsk@gmail.com>
2017-03-31 15:14:12 +0300

    Add polyfill for pics in ie & js menu toggler

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
-// var navMain = document.querySelector('.main-nav');
-// var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+var header = document.querySelector('.page-header');
 
-// navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--nojs');
+header.classList.remove('page-header--nojs');
 
-// navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', function() {
 
-//   if (navMain.classList.contains('main-nav--closed')) {
-//     navMain.classList.remove('main-nav--closed');
-//     navMain.classList.add('main-nav--opened');
-//   }
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  }
 
-//   else {
-//     navMain.classList.add('main-nav--closed');
-//     navMain.classList.remove('main-nav--opened');
-//   }
-// });
+  else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+
+  if (header.classList.contains('page-header--closed')) {
+    header.classList.remove('page-header--closed');
+    header.classList.add('page-header--opened');
+  }
+
+  else {
+    header.classList.remove('page-header--opened');
+    header.classList.add('page-header--closed');
+  }
+});
