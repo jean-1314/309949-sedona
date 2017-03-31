7dce85a3ca15e9c85a9b851ca3522079037cc5ba
jean-1314 <isfiremsk@gmail.com>
2017-03-31 17:18:13 +0300

    Add form onsubmit modal element

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 var header = document.querySelector('.page-header');
+var submitBtn = document.querySelector('.btn-submit');
+var onSubmit = document.querySelector('.form-onsubmit');
+var submitClose = document.querySelector('.form-onsubmit__btn');
 
 navMain.classList.remove('main-nav--nojs');
 header.classList.remove('page-header--nojs');
@@ -26,4 +29,20 @@ navToggle.addEventListener('click', function() {
     header.classList.remove('page-header--opened');
     header.classList.add('page-header--closed');
   }
+});
+
+submitBtn.addEventListener('click', function() {
+  event.preventDefault();
+
+  if (onSubmit.classList.contains('form-onsubmit--closed')) {
+    onSubmit.classList.remove('form-onsubmit--closed');
+  }
+
+  else {
+    onSubmit.classList.add('form-onsubmit--closed');
+  }
+});
+
+submitClose.addEventListener('click', function() {
+  onSubmit.classList.add('form-onsubmit--closed');
 });
